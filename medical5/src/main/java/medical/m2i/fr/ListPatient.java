9d85003f44ceee9534492388d673ccfe4a72bb48
Amine <resa@booking-tunisie.com>
2021-10-28 16:21:55 +0100

    patients listing

---
+++ medical5/src/main/java/medical/m2i/fr/ListPatient.java
@@ -0,0 +1,68 @@
+package medical.m2i.fr;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import medical.m2i.model.Patient;
+
+/**
+ * Servlet implementation class ListPatient
+ */
+@WebServlet("/listpatient")
+public class ListPatient extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ListPatient() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Patient pliste = new Patient();
+		
+		
+		response.setContentType(" text / html ");
+		PrintWriter out = response.getWriter();
+		out.println("<html ><head><meta charset=\"UTF-8\"><title>Liste des patients</title></head>");
+		// Charger la liste des patients
+		out.println("<body>");
+		
+		out.println("<h1>Liste des patients</h1>");
+		
+		out.println("<table class='table' border='1'>");
+		out.println("<tr>");
+		out.println("<td>ID</td><td>Nom & prénom</td><td>Date de naissance</td><td>Adresse</td><td>Ville</td><td>Pays</td>");
+		out.println("</tr>");
+		
+		try {
+			for( Patient p : pliste.getPatients() ) {
+				out.println("<tr>");
+				out.println("<td>1</td><td>"+p.getNom() + " "+p.getPrenom()+"</td><td>"+p.getLocalDatenaissance()+"</td><td>"+p.getAdresse()+"</td><td>"+p.getVille()+"</td><td>"+p.getPays()+"</td>");
+				out.println("</tr>");
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		out.println("</table>");
+		
+		out.println("</body>");
+		
+		out.println("</html>");
+	}
+
+}
--- medical5/src/main/java/medical/m2i/model/Patient.java
+++ medical5/src/main/java/medical/m2i/model/Patient.java
@@ -4,8 +4,11 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Patient {
 	private String nom;
@@ -14,10 +17,8 @@ public class Patient {
 	private String adresse;
 	private String pays;
 	private String ville;
-	
-	
-	public Patient(String nom, String prenom, Date datenaissance, String adresse, 
-			String pays, String ville) {
+
+	public Patient(String nom, String prenom, Date datenaissance, String adresse, String pays, String ville) {
 		this.nom = nom;
 		this.prenom = prenom;
 		this.datenaissance = datenaissance;
@@ -25,64 +26,98 @@ public class Patient {
 		this.pays = pays;
 		this.ville = ville;
 	}
-	
+
+	public Patient() {
+		// TODO Auto-generated constructor stub
+	}
+
 	public String getNom() {
 		return nom;
 	}
+
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+
 	public String getPrenom() {
 		return prenom;
 	}
+
 	public void setPrenom(String prenom) {
 		this.prenom = prenom;
 	}
+
 	public Date getLocalDatenaissance() {
 		return datenaissance;
 	}
+
 	public void setLocalDatenaissance(Date datenaissance) {
 		this.datenaissance = datenaissance;
 	}
+
 	public String getAdresse() {
 		return adresse;
 	}
+
 	public void setAdresse(String adresse) {
 		this.adresse = adresse;
 	}
+
 	public String getPays() {
 		return pays;
 	}
+
 	public void setPays(String pays) {
 		this.pays = pays;
 	}
+
 	public String getVille() {
 		return ville;
 	}
+
 	public void setVille(String ville) {
 		this.ville = ville;
 	}
-	
-	
+
 	public void savePatient() throws ClassNotFoundException, SQLException {
-		Class.forName("com.mysql.cj.jdbc.Driver");  
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/medical5?serverTimezone=UTC", "root",
+				"");
+
+		PreparedStatement stmt = con.prepareStatement(
+				"INSERT INTO `patient` (`nom`, `prenom`, `datenaissance`, `adresse`, `ville`, `pays`) "
+						+ "VALUES ( ? , ? , ? , ? , ? , ? ) ");
+
+		stmt.setString(1, nom);
+		stmt.setString(2, prenom);
+		stmt.setDate(3, datenaissance);
+		stmt.setString(4, adresse);
+		stmt.setString(5, ville);
+		stmt.setString(6, pays);
+
+		stmt.executeUpdate();
+
+	}
+
+	public List<Patient> getPatients() throws Exception {
+		Class.forName("com.mysql.cj.jdbc.Driver");    
 		Connection con= DriverManager.getConnection( "jdbc:mysql://localhost:3306/medical5?serverTimezone=UTC","root","");  
 		
+		//here sonoo is database name, root is username and password  
+		Statement stmt=con.createStatement();  
+		ResultSet rs=stmt.executeQuery("select * from patient");  
 		
-		PreparedStatement stmt=con.prepareStatement( "INSERT INTO `patient` (`nom`, `prenom`, `datenaissance`, `adresse`, `ville`, `pays`) "
-				+ "VALUES ( ? , ? , ? , ? , ? , ? ) " );
+		List<Patient> lp = new ArrayList<Patient>();
 		
+		Patient p;
+		while(rs.next()) { 
+			p = new Patient( rs.getString("nom") , rs.getString("prenom") , rs.getDate("datenaissance") , rs.getString("adresse") , rs.getString("pays") , rs.getString("ville") );
+			lp.add(p);
+		}
 		
-		stmt.setString(1, nom );
-		stmt.setString(2, prenom );
-		stmt.setDate(3, datenaissance );
-		stmt.setString(4, adresse );
-		stmt.setString(5, ville );
-		stmt.setString(6, pays );
+		con.close(); 
 		
-		stmt.executeUpdate();
-		
+		return lp; 
 	}
-	
-	
+
 }
